f3c8474f75217c7a102956f64a9ac85969ae33bd
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-07-15 16:27:08 -0400

    got some tests passinggit add .

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,27 @@
+#require 'rack-flash'
+
+class SongsController < ApplicationController
+    #use Rack::Flash 
+
+    get '/songs' do
+        @songs = Song.all 
+        erb :'/songs/index' 
+    end
+
+    get '/songs/new' do
+        erb :'/songs/new' #show new songs view
+    end
+
+    get '/songs/:slug' do
+        @song = Song.find_by_slug(params[:slug])
+        erb :'/songs/show'
+    end
+
+
+
+
+
+
+end
+
+
